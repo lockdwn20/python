4950d6e010f01645bed28c78f8232e3800674be5
Ray Race <lockdwn20@gmail.com>
2017-12-09 15:01:41 -0500

    fixed idents

--- if.py
+++ if.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 x = int(input("Please enter an integer: "))
-    if x < 0:
-        x = 0
-        print('Negative changed to zero')
-    elif x == 0:
-        print('Zero')
-    elif x == 1:
-        print('Single')
-    else:
-        print('More')
+if x < 0:
+    x = 0
+    print('Negative changed to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
