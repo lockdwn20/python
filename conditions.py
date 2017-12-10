2d75afb3fe014198090081bb3873cff1a7203c32
Ray Race <lockdwn20@gmail.com>
2017-12-10 15:42:39 -0500

    adjusted again

--- conditions.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-string1, string2, string3 = '', 'Trondheim', 'Hammer Dance'
-non_null = string1 or string2 or string3
-print(non_null)
-print(non_null)
-print(non_null)
--- queues.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from collections import deque
-queue = deque(["Eric", "John", "Michael"])
-queue.append("Terry")
-queue.append("Graham")
-print(queue.popleft())
-print(queue.popleft())
-print(queue)
